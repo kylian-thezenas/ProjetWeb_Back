748d4097111979a3419550620c6e7bfe09515a6c
Kylian Thezenas <kylian.thezenas@etu.umontpellier.fr>
2023-05-13 16:29:04 +0200

    maj route  user

--- Back/routes/userRoutes.js
+++ Back/routes/userRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 router.get('/api/user/', userController.alluser);
-router.post('/api/user/', userController.addTUser);
+router.put('/api/user/', userController.connexion);
+router.post('/api/user/', userController.inscription);
 router.get('/api/user/:name', userController.getUser);
 router.put('/api/user/:name', userController.updateUser);
 router.delete('/api/user/:name', userController.deleteUser);
